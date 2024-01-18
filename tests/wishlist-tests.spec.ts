6b43f42bb35e7bad7d9e1355c25909058259bff9
waaed azzam <waaed013@gmail.com>
2024-01-18 14:11:36 +0200

    fix teardown

--- tests/wishlist-tests.spec.ts
+++ tests/wishlist-tests.spec.ts
@@ -11,26 +11,31 @@ import { buildWishlistDeleteItemRequest } from "../logic/api/request-body/wishli
 test.describe("Wishlist Tests", async () => {
 
     let browser: BrowserWrapper;
-    let itemId:number
-    
+    let tearDown: (() => Promise<any>)[] = []
+
     test.beforeEach(async () => {
         browser = new BrowserWrapper();
     });
 
     test.afterEach(async () => {
-        const apiCall = new ApiCalls();
-        const data = buildWishlistDeleteItemRequest(itemId)
-        const response = await apiCall.deleteItemFromWishlist(data)
+        // const apiCall = new ApiCalls();
+        // const data = buildWishlistDeleteItemRequest(itemId)
+        // const response = await apiCall.deleteItemFromWishlist(data)
+        while (tearDown.length > 0) {
+            tearDown.pop()!()
+        }
         await browser.closeBrowser()
     })
 
-    test('Add Item To Wishlist Via Api And Validate Via Ui', async () => {
+    test.only('Add Item To Wishlist Via Api And Validate Via Ui', async () => {
         const data = buildWishlistRequest(productConfig.product1.sku)
         const apiCall = new ApiCalls()
         const response = await apiCall.addItemToWishlist(data)
-        itemId = response.data.data.addProductsToWishlist.anyWishlist.items[0].id
+        const itemId = response.data.data.addProductsToWishlist.anyWishlist.items[0].id
+        const data1 = buildWishlistDeleteItemRequest(itemId)
+        tearDown.push(async () => apiCall.deleteItemFromWishlist(data1))
         const page = await browser.getPage(uiUrls.wishlistPage)
-        await page.waitForTimeout(5000)
+        //await page.waitForTimeout(5000)
         const wishlist = new WishlistPage(page)
         await expect(wishlist.getItemTitleLocator(productConfig.product1.name)).toBeVisible()
     });
